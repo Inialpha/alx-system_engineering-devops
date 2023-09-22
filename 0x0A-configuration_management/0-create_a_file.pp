2c61bca06ce13eba2b9cfde33e8d93d21a10e193
Initech <inimfonebong001@gmail.com>
2023-09-22 03:50:29 -0700

    Add task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file /tmp/school
+
+
+file { 'tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
