492e45be55bb560cf0f9e643b8755ff4d4f430a4
Initech <inimfonebong001@gmail.com>
2023-09-23 07:12:49 -0700

    Add task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# SSH client configuration
+include stdlib
+
+file_line { 'authentication file':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
+}
+
+
+file_line { 'refuse password':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
+}
