b698e1c5a4c187361b0473969a745364439d325a
Initech <inimfonebong001@gmail.com>
2023-10-23 03:34:36 -0700

    Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that print information about an employee"""
+import sys
+import requests
+
+user_id = sys.argv[1]
+user = requests.get("https://jsonplaceholder.typicode.com/users/"
+                    + user_id).json()
+todos = requests.get("https://jsonplaceholder.typicode.com/todos/",
+                     params={'userId': user_id}).json()
+completed = [todo.get('title') for todo in todos if todo.get("completed")]
+print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+      len(completed), len(todos)))
+
+for task in completed:
+    print("\t {}".format(task))
