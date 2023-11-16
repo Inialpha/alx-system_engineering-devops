916ec0559d647ebcd7491dce50a430d090b24a89
Initech <inimfonebong001@gmail.com>
2023-11-16 13:28:36 +0000

    change

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # increase f8le limit for holberton user
 
 exec {' increase soft':
-command => "/bin/sed -i 's/holberton hard nofile 5/holberton soft 5000/' /etc/security/limits.conf",
+command => "/bin/sed -i 's/holberton soft nofile 5/holberton soft nofile 5000/' /etc/security/limits.conf",
 }
 
 exec {' increase hard':
-command => "/bin/sed -i 's/holberton hard nofile 4/holberton hard 5000/' /etc/security/limits.conf",
+command => "/bin/sed -i 's/holberton hard nofile 4/holberton hard nofile 5000/' /etc/security/limits.conf",
 }
